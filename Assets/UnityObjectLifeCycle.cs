c689a668a7f612c8c3ff0c03d953fb404970c96f
seungin <seungin.cha@gmail.com>
2019-12-17 17:18:10 +0900

    Modify Unity Object LifeCycle

--- Assets/UnityObjectLifeCycle.cs
+++ Assets/UnityObjectLifeCycle.cs
@@ -1,46 +1,55 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Reflection;
 using UnityEngine;
 
 public class UnityObjectLifeCycle : MonoBehaviour
 {
 	void Awake()
 	{
-		Debug.Log("Invoked once when creating a game object.");
+		string name = MethodBase.GetCurrentMethod().Name;
+		Debug.Log(name + ": 게임 오브젝트가 생성될 때 실행");
 	}
 
 	void OnEnable()
 	{
-		Debug.Log("Invoked when the game object is activated.");
+		string name = MethodBase.GetCurrentMethod().Name;
+		Debug.Log(name + ": 게임 오브젝트가 활성화 되었을 때 실행");
 	}
 
 	void Start()
 	{
-		Debug.Log("Invoked just before updating the game object.");
+		string name = MethodBase.GetCurrentMethod().Name;
+		Debug.Log(name + ": 업데이트 시작 직전에 실행");
 	}
 
 	void FixedUpdate()
 	{
-		Debug.Log("Update physics for fixed framerate.");
+		string name = MethodBase.GetCurrentMethod().Name;
+		Debug.Log(name + ": 물리 연산 업데이트, 고정된 주기로 CPU를 많이 사용");
 	}
 
 	void Update()
 	{
-		Debug.Log("Update game logics.");
+		string name = MethodBase.GetCurrentMethod().Name;
+		Debug.Log(name + ": 게임 로직 업데이트, 환경에 따라 실행주기가 떨어질 수 있음");
 	}
 
 	void LateUpdate()
 	{
-		Debug.Log("Invoked after Update().");
+		string name = MethodBase.GetCurrentMethod().Name;
+		Debug.Log(name + ": 모든 업데이트가 끝난 후 실행");
 	}
 
 	void OnDisable()
 	{
-		Debug.Log("Invoked when the game object is deactivated.");
+		string name = MethodBase.GetCurrentMethod().Name;
+		Debug.Log(name + ": 게임 오브젝트가 비활성화 되었을 때 실행");
 	}
 
 	void OnDestroy()
 	{
-		Debug.Log("Invoked when deleting the game object.");
+		string name = MethodBase.GetCurrentMethod().Name;
+		Debug.Log(name + ": 게임 오브젝트가 삭제될 때 실행");
 	}
 }
