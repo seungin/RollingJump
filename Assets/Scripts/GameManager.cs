84725c247b3dfb039f5b813c661a2d3de8558a0a
seungin <seungin.cha@gmail.com>
2019-12-23 21:39:10 +0900

    Add Cliff Detection

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,9 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
 	public int totalItemCount;
 	public int stage;
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Player")
+		{
+			Debug.Log("Restart this stage: " + stage);
+			SceneManager.LoadScene(stage);
+		}
+	}
 }
